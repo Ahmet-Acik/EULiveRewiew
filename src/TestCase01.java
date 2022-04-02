e02cbeca479b8bbac8d0371f3259a8b436470788
Ahmet-Acik <a.acik@icloud.com>
2022-04-02 20:31:09 +0100

    second commit

--- src/TestCase01.java
+++ src/TestCase01.java
@@ -3,5 +3,10 @@ public class TestCase01 {
     public static void main(String[] args) {
 
         System.out.println(" Testing with Chrome Browser ");
+        System.out.println(" Testing with Chrome Browser ");
+        System.out.println(" Testing with Chrome Browser ");
+        System.out.println(" Testing with Chrome Browser ");
+        System.out.println(" Testing with Chrome Browser ");
+        System.out.println(" Testing with Chrome Browser ");
     }
 }
